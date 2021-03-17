d4afaa2333a9d322ddce9764ccf56150eeff0210
James Meneses <james.meneseswk@gmail.com>
2021-03-17 11:59:22 -0300

    Testing args

---
+++ high-order.js
@@ -0,0 +1,13 @@
+/* At first we are testing */
+function noisy (f) {
+    return (...args) => {
+        console.log('calling with ', args);
+        let result = f(...args);
+        console.log('called with', args, ', returned ', result)
+        return result;
+    }
+}
+
+noisy(Math.min)(3,2,1);
+
+["a", "b"].forEach(l => console.log(l))
