b75118a5b3a1e306897b9b5b91384c3f8418b809
James Meneses <james.meneseswk@gmail.com>
2021-03-17 12:49:14 -0300

    Implementando map - high order functions

---
+++ map.js
@@ -0,0 +1,47 @@
+/* Here we map all shit - by @james-meneses aka yung_jaimo */
+
+let singers = [{
+    name : 'Travis Scott',
+    age: 27, 
+    alive: true,
+    genre: 'rap',
+    rate: 5
+}, {
+    name: 'Chorão',
+    age: 30,
+    alive: false,
+    genre: 'rock',
+    rate: 5
+}, {
+    name: 'Mr. Catra',
+    age: 45,
+    genre: 'brazilian funk',
+    alive: false, 
+    rate: 5
+}, {name: 'Manu Gavassi',
+    age: 27,
+    genre: 'shit',
+    rate: 1,
+    alive: true
+}, {
+    name: 'Jimmi Hendrix',
+    age: 27,
+    genre: 'rock',
+    rate: 5, 
+    alive: false
+}]
+
+function map ( array, transform ) {
+    let mapped = []
+    for ( let el of array ) {
+        mapped.push(transform(el))
+    }
+    return mapped
+}
+
+
+let singersAging = singer => singer.age + 1
+
+console.log ('Singers age +1 - ', map(singers, singersAging))
+
+/* não dá pra tirar muito disso kk, mas só para aprender mesmo */
