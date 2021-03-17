9c5e1f74012dae9e173c99cedf06498347c53632
James Meneses <james.meneseswk@gmail.com>
2021-03-17 16:28:33 -0300

    Some more practice with filter, map and reduce

--- high-order-exercises.js
+++ high-order-exercises.js
@@ -5,3 +5,22 @@ let arrays = [[1, 2, 3], [4, 5], [6]];
 console.log (arrays.reduce((total, item) => total.concat(item)))
 
 // → [1, 2, 3, 4, 5, 6]
+
+
+/* daqui pra baixo é só pra eu praticar mesmo kk */
+let numbers = [ 3, 8 , 13, 18, 21]
+console.log('numeros =>', numbers);
+
+let sum = (a, b) => a + b
+
+console.log( 'todos numeros somados => ', numbers.reduce(sum) )
+
+let pares = numbers.filter(n => n % 2 == 0)
+// só pares
+console.log( 'pares => ', pares.reduce(sum) )
+
+let doubleImpares = numbers.filter( n => n%2 == 1).map(n => n*2)
+console.log('impares x2 => ', doubleImpares)
+
+/* voltando.. */
+
