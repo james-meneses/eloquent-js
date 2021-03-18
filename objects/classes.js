9dd86544caac8e368c006a035a7baa29444ce2c8
James Meneses <james.meneseswk@gmail.com>
2021-03-18 12:12:25 -0300

    Symbol()

--- objects/classes.js
+++ objects/classes.js
@@ -37,3 +37,36 @@ console.log ( Object.getPrototypeOf (Coelho) === Function.prototype )
 
 // o prototype do objeto (instância) cardiCoelho é o Coelho.prototype
 console.log ( Object.getPrototypeOf (cardiCoelho) === Coelho.prototype )
+
+Coelho.prototype.toString = function () {
+    console.log('Sou o coelho ' + this.tipo )
+}
+
+
+let coelhoFofao = new Coelho('fofao');
+
+// string() vai chamar o .toString() do Coelho, que tem retorno diferente do original (Object.toString())
+String(coelhoFofao)
+
+/* Symbols */
+
+let sym = Symbol ("name")
+Coelho.prototype[sym] = 'Jaime'
+console.log(coelhoFofao[sym])
+
+
+const toStringSymbol = Symbol("toString")
+
+Array.prototype[toStringSymbol] = function () {
+    return this.length + ' values'
+}
+
+console.log([1,2].toString())
+
+console.log([1,2][toStringSymbol]())
+
+let meuTexto = {
+    [toStringSymbol]() { return 'O telefone tocou novamente...' }
+}
+
+console.log(meuTexto[toStringSymbol]())
